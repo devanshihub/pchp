f65b2c139a65a8124e0ea94a87351bdbf9acb477
devanshihub <devanshimehrotra2024@gmail.com>
2024-08-24 20:20:18 +0530

    Create question_1.java

---
+++ question_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String k[])
+    {
+        Scanner ob=new Scanner (System.in);
+        System.out.print("\nenter a number : ");
+        int n=ob.nextInt();
+        int r=0,sum_of_digits=0;
+        while(n>0)
+        {
+              r=n%10;
+              sum_of_digits=sum_of_digits+r;
+              n=n/10;
+        }
+
+        System.out.print("\nsum of digits  is  "+sum_of_digits);
+
+    }
+}
